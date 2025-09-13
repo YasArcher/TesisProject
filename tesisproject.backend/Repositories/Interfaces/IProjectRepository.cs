c74ce15148569c790d32c6d1e8ab1ba25b143868
Marlon Masabanda <marlon25.masabanda@gmail.com>
2025-09-13 01:29:57 -0500

    refactor(backend): replace entity usage with DTOs in service layer

--- tesisproject.backend/Repositories/Interfaces/IProjectRepository.cs
+++ tesisproject.backend/Repositories/Interfaces/IProjectRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using tesisproject.shared.DTOs.Project.Response;
 using tesisproject.shared.Entities.Core;
 
 namespace tesisproject.backend.Repositories.Interfaces
