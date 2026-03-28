6978d94e29a31b2552c6522a9340d99c063fc8aa
Marlon Masabanda <marlon25.masabanda@gmail.com>
2026-03-27 21:40:12 -0500

    refactor: Update TechnicianGroupsTab to use Container component

--- tesisproject.frontend/SharedUI/Card/CardBody.razor.cs
+++ tesisproject.frontend/SharedUI/Card/CardBody.razor.cs
@@ -2,8 +2,15 @@
 
 namespace tesisproject.frontend.SharedUI.Card
 {
-    public partial class CardBody
+    public partial class CardBody : ComponentBase
     {
         [Parameter] public RenderFragment? ChildContent { get; set; }
+        [Parameter] public string? Class { get; set; }
+        [Parameter] public bool Scroll { get; set; } = false;
+
+        private string _classes =>
+            Scroll
+                ? $"px-4 py-2 flex flex-col flex-1 min-h-0 overflow-y-auto {Class}"
+                : $"px-4 py-2 flex flex-col flex-1 min-h-0 overflow-hidden {Class}";
     }
 }
